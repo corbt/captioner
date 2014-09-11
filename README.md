d5a1c57df0051c8b1a7085660827e7f9880634d8
Kyle Corbitt <kylecorbitt@gmail.com>
2014-09-11 12:17:07 +0100

    first barely workable version

---
+++ README.md
@@ -0,0 +1,47 @@
+# Captioner
+
+## Overview
+
+This is a very simple utility for creating slide decks from images and videos. Images can have an optional caption. That's it!
+
+## Dependencies
+
+This project has been tested with Ruby 2.1. It may work with older versions.
+
+There are no gem dependencies for this utility. However, it does depend on [imagemagick](http://www.imagemagick.org/) being available on the command line.
+
+## Usage
+
+`ruby caption.rb [caption_config_file].yml`
+
+## Caveats
+
+Currently there is absolutely 0 attempt to be responsive. This was designed to caption screenshots on my 13" Retina Macbook Pro and captions are placed using absolute coordinates, so images at alternative resolutions are unlikely to work well with the captioning functionality.
+
+Example config file: 
+
+```yaml
+---
+
+in_dir: media
+out_dir: media_out
+
+media:
+  - image: homepage.png
+    caption: Memorials are public and searchable from the homepage.
+  - image: memorial.png
+    caption: Each memorial includes a collection of videos from friends and family.
+  - image: memorial_admin.png
+    caption: Administrators can change memorial settings and hide recordings.
+  - image: home_record.png
+    caption: Loved ones can also record videos from home on a phone or tablet.
+  - video: mark_cluff.mp4
+  - image: fh_admin.png
+    caption: The funeral home admin interface allows you to edit memorials and export data.
+```
+
+ * `in_dir` [optional] the root directory your media is found in. By default this is "media"
+ * `out_dir` [optional] the directory you wish your slide deck to be saved to. Any existing files in this directory will be deleted. Default is `in_dir` with the postfix `_out`.
+ * `media`: An in-order list of media files (video or audio) to use in the slide deck
+ * `image` or `video`: the name/path to the image or video within the input directory
+ * `caption` [image only] the text you wish to 
--- caption.rb
+++ caption.rb
@@ -1,8 +1,8 @@
-require 'rubygems'
-require 'bundler/setup'
+# require 'rubygems'
+# require 'bundler/setup'
 
-# require your gems as usual
 require 'yaml'
+require 'fileutils'
 
 # Borrowed from the Rails source
 def word_wrap(text, line_width: 80)
@@ -11,15 +11,23 @@ def word_wrap(text, line_width: 80)
   end * "\n"
 end
 
-def process_image config
-  caption = word_wrap config['caption']
+def copy_file name, index
+  extension = name.split('.').last
+  FileUtils.cp File.join($config['in_dir'], name),
+               File.join($config['out_dir'], index.to_s + "." + extension)
+end
+
+def process_image img_data
+  copy_file img_data['image'], img_data['index'] and return unless img_data['caption']
+
+  caption = word_wrap img_data['caption']
   options = [
     '-size 2500x200 xc:none',
     '-gravity center',
     '-stroke black',
     '-strokewidth 10',
     '-pointsize 60',
-    '-font helvetica',
+    # '-font helvetica',
     '-annotate', "0 \"#{caption}\"",
     '-background none',
     '-shadow 300x10+0+0',
@@ -27,21 +35,29 @@ def process_image config
     '-stroke none',
     '-fill', 'white',
     '-annotate', "0 \"#{caption}\"",
-    "\"#{File.join("images", config['image'])}\"",
+    "\"#{File.join($config['in_dir'], img_data['image'])}\"",
     '+swap',
     '-gravity south',
     '-geometry +0-3',
-    '-composite', File.join('out', "#{config['index']}.png")
+    '-composite', File.join($config['out_dir'], "#{img_data['index']}.png")
   ].join ' '
 
   `convert #{options}`
 end
 
-config = YAML.load_file 'config.yml'
-FileUtils.rm_r 'out'
-FileUtils.mkdir_p 'out'
+def process_video video_data
+  copy_file video_data['video'], video_data['index']
+end
 
-config['media'].each_with_index do |config, i|
-  config['index'] = i 
-  process_image(config) if config.has_key? 'image'
+$config = YAML.load_file ARGF.argv.first
+$config['in_dir'] = $config['in_dir'] || "_media"
+$config['out_dir'] = $config['out_dir'] || "#{$config['in_dir']}_out"
+
+FileUtils.rm_rf $config['out_dir']
+FileUtils.mkdir_p $config['out_dir']
+
+$config['media'].each_with_index do |media_data, i|
+  media_data['index'] = i 
+  process_image(media_data) if media_data.has_key? 'image'
+  process_video(media_data) if media_data.has_key? 'video'
 end
